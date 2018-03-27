7f80546708a1adb6b2b320b8afdad441f3ea7f44
lianghongquan <13@qq.com>
2018-03-27 23:46:53 +0800

    收银台的controller修改成@RequestMapping("view")

--- src/main/java/cn/wolfcode/crm/web/controller/CheckOutController.java
+++ src/main/java/cn/wolfcode/crm/web/controller/CheckOutController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("checkout")
 public class CheckOutController {
-    @RequestMapping("query")
-    //这个标标签表示只要有一个权限就能访问
+    @RequestMapping("view")
     public String view(){
         return "checkout/checkout";
     }
