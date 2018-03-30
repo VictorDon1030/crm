048d39e5522c17212e6c8d0201641fd13b764a4b
Leslie <Leslie@163.com>
2018-03-30 14:19:14 +0800

    Merge branch 'admin1' of 192.168.113.47:wolfcatcher/dece
    # Conflicts:
    #	src/main/java/cn/wolfcode/crm/web/controller/ClassificationController.java

--- src/main/java/cn/wolfcode/crm/web/controller/ProductController.java
+++ src/main/java/cn/wolfcode/crm/web/controller/ProductController.java
@@ -126,134 +126,26 @@ public class ProductController {
             //设置单元格内容
             //cell.setCellValue("你好");
             row.createCell(0).setCellValue(product.getName());
-            row.createCell(1).setCellValue(product.getBrand());
-            row.createCell(2).setCellValue(product.getAuxiliaryWord());
-            row.createCell(3).setCellValue(product.getGoodsMark());
-            row.createCell(4).setCellValue(product.getPurchasingPrice().toString());
-            row.createCell(5).setCellValue(product.getUnitpPrice().toString());
-            row.createCell(6).setCellValue(product.getMemberPrice().toString());
-            row.createCell(7).setCellValue(product.getMinDiscount().toString());
-            row.createCell(8).setCellValue(product.getMinPrice().toString());
-            row.createCell(9).setCellValue(product.getInitialInventory());
-            row.createCell(10).setCellValue(product.getSpecification());
-            row.createCell(11).setCellValue(product.getUnit());
-            row.createCell(12).setCellValue(product.getIntegral());
-            row.createCell(13).setCellValue(product.getCommission().toString());
-            row.createCell(14).setCellValue(product.getPastDueTime());
-            row.createCell(15).setCellValue(product.getRemark());
-            row.createCell(16).setCellValue(product.getImagePath());
+            row.createCell(1).setCellValue(product.getName());
+            row.createCell(2).setCellValue(product.getBrand());
+            row.createCell(3).setCellValue(product.getAuxiliaryWord());
+            row.createCell(4).setCellValue(product.getGoodsMark());
+            row.createCell(5).setCellValue(product.getPurchasingPrice().toString());
+            row.createCell(6).setCellValue(product.getUnitpPrice().toString());
+            row.createCell(7).setCellValue(product.getMemberPrice().toString());
+            row.createCell(8).setCellValue(product.getMinDiscount().toString());
+            row.createCell(9).setCellValue(product.getMinPrice().toString());
+            row.createCell(10).setCellValue(product.getInitialInventory());
+            row.createCell(11).setCellValue(product.getSpecification());
+            row.createCell(12).setCellValue(product.getUnit());
+            row.createCell(13).setCellValue(product.getIntegral());
+            row.createCell(14).setCellValue(product.getCommission().toString());
+            row.createCell(15).setCellValue(product.getPastDueTime());
+            row.createCell(16).setCellValue(product.getRemark());
+            row.createCell(17).setCellValue(product.getImagePath());
         }
         //写入数据(输出到浏览器)
         wb.write(response.getOutputStream());
     }
 
-   /* //导入
-    @RequestMapping("importXls")
-    public void importXls(MultipartFile file, HttpServletResponse response) throws Exception{
-        InputStream inputStream = file.getInputStream();
-        //创建excel文件 指定输入流
-        HSSFWorkbook wb = new HSSFWorkbook(inputStream);
-
-        Sheet sheet = wb.getSheetAt(0);
-        //获取最后一个索引
-        int lastRowNum = sheet.getLastRowNum();
-        for (int i = 1; i <= lastRowNum; i++) {
-            Product product = new Product();
-            Row row = sheet.getRow(i);
-            try {
-                product.setName(getContent(row.getCell(0)).toString());
-                product.setBrand(getContent(row.getCell(1)).toString());
-                product.setAuxiliaryWord(getContent(row.getCell(2)).toString());
-                product.setGoodsMark(getContent(row.getCell(3)).toString());
-                product.setPurchasingPrice(getContent(row.getCell(3)).toString());
-                product.setUnitpPrice(getContent(row.getCell(3)).toString());
-                product.setMemberPrice(getContent(row.getCell(3)).toString());
-                product.setMinDiscount(getContent(row.getCell(3)).toString());
-                product.setMinPrice(getContent(row.getCell(3)).toString());
-                product.setInitialInventory(getContent(row.getCell(3)).toString());
-                product.setSpecification(getContent(row.getCell(3)).toString());
-                product.setUnit(getContent(row.getCell(3)).toString());
-                product.setIntegral(getContent(row.getCell(3)).toString());
-                product.setCommission(getContent(row.getCell(3)).toString());
-                product.setPastDueTime(getContent(row.getCell(3)).toString());
-                product.setRemark(getContent(row.getCell(3)).toString());
-                product.setImagePath(getContent(row.getCell(3)).toString());
-                productService.saveOrUpdate(product);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
-    public Object getContent(Cell cell){
-        switch (cell.getCellType()){
-            case 1:
-                return cell.getRichStringCellValue().getString();
-            case 0:
-                return cell.getNumericCellValue();
-            default:
-        }
-        return null;
-    }*/
-
-    //查询分类商品列
-    @RequestMapping("fruits")
-    @ResponseBody
-    public Object fruits(Long id){
-        return productService.selectByUnitId(1L);
-    }
-    @RequestMapping("drinks")
-    @ResponseBody
-    public Object drinks(Long id){
-        return productService.selectByUnitId(2L);
-    }
-    @RequestMapping("foodstuff")
-    @ResponseBody
-    public Object foodstuff(Long id){
-        return productService.selectByUnitId(3L);
-    }
-    @RequestMapping("vegetables")
-    @ResponseBody
-    public Object vegetables(Long id){
-        return productService.selectByUnitId(4L);
-    }
-    @RequestMapping("grain")
-    @ResponseBody
-    public Object grain(Long id){
-        return productService.selectByUnitId(5L);
-    }
-    @RequestMapping("aquatic")
-    @ResponseBody
-    public Object aquatic(Long id){
-        return productService.selectByUnitId(6L);
-    }
-    @RequestMapping("child")
-    @ResponseBody
-    public Object child(Long id){
-        return productService.selectByUnitId(7L);
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
