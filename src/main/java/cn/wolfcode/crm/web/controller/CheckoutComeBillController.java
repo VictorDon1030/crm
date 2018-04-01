38a4b87364b702b7b87856abbc40568b5cbadb3a
admin1 <860708931@qq.com>
2018-04-01 09:35:31 +0800

    修复时间BUG 外加最近热卖产品///

--- src/main/java/cn/wolfcode/crm/web/controller/CheckoutComeBillController.java
+++ src/main/java/cn/wolfcode/crm/web/controller/CheckoutComeBillController.java
@@ -5,11 +5,8 @@ import cn.wolfcode.crm.service.ICheckoutComeBillService;
 import cn.wolfcode.crm.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("checkoutComeBill")
